4d8b235822d201f3bc53e4ce8fdbdcbc86abaa2a
namugarurasyda <2100813912@std.kyu.ac.ug>
2026-06-05 00:44:26 +0300

    Arrow function

--- src/components/func.components.jsx
+++ src/components/func.components.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 
 
-function Greet(){
-    return (
-        <div>
-            <h1>Hello, welcome to React with Javascript!</h1>
-        </div>
-    )
+// function Greet(){
+//     return (
+//         <div>
+//             <h1>Hello, welcome to React with Javascript!</h1>
+//         </div>
+//     )
+// }
+const Greet = ()  =>{
+    return<h1> Hello RCR week 2</h1>
 }
 
  export default Greet
