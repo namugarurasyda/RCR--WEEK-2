3fcf5a1fc5943fbda938decab4fcc4ef3e41f06e
namugarurasyda <2100813912@std.kyu.ac.ug>
2026-06-05 00:36:31 +0300

    Adding the  functional component

---
+++ src/components/func.components.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+function Greet(){
+    return (
+        <div>
+            <h1>Hello, welcome to React with Javascript!</h1>
+        </div>
+    )
+}
+
+ export default Greet
