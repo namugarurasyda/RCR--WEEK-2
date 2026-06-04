973baac71969c493401bb6df3a1cd03fbcf6f03a
namugarurasyda <2100813912@std.kyu.ac.ug>
2026-06-05 01:30:54 +0300

    Change

--- src/components/hello.jsx
+++ src/components/hello.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 
 const Hello = () => {
-    return (
-        <div>
-            <h1>Hello, welcome to React with Javascript!</h1>
-        </div>
-    )
+    // return (
+    //     <div>
+    //         <h1>Hello, welcome to React with Javascript!</h1>
+    //     </div>
+    // )
+    return React.createElement(
+         'div', 
+         null,  
+         React.createElement('h1', null, 'Hello RCR week  with arrow function'))
 } 
 
 export default Hello
