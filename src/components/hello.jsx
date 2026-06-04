a7217ba689c28e39f933819bac7d7649349240e4
namugarurasyda <2100813912@std.kyu.ac.ug>
2026-06-05 01:21:48 +0300

    Explanation of jsx

---
+++ src/components/hello.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hello = () => {
+    return (
+        <div>
+            <h1>Hello, welcome to React with Javascript!</h1>
+        </div>
+    )
+} 
+
+export default Hello
