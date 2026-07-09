c82aab2be26ccd82b717a0086a224b2f7db212ef
namugarurasyda <2100813912@std.kyu.ac.ug>
2026-07-10 01:22:47 +0300

    Adding the  Routes

--- src/components/hello.jsx
+++ src/components/hello.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router";
+import { BrowserRouter, Routes, Route } from "react-router";
 
 
 const root = document.getElementById("root");
