101a67b4eb593d840f124137bfe8e0b56ebb00aa
namugarurasyda <2100813912@std.kyu.ac.ug>
2026-07-10 00:35:48 +0300

    Adding the BrowserRouter

--- src/components/hello.jsx
+++ src/components/hello.jsx
@@ -2,10 +2,18 @@ import React from 'react'
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router";
 
+
+const root = document.getElementById("root");
+
+ReactDOM.createRoot(root).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
 const Hello = props => {
     console.log(props)
 
-    
+
 
     return (
         <div>
